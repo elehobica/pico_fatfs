339e3392acd01cc4c867d84174cb5613a992162a
elehobica <elehobica@gmail.com>
2024-05-15 13:01:33 +0000

    add pico_fatfs_reboot_spi()

--- tf_card.h
+++ tf_card.h
@@ -40,6 +40,7 @@ extern "C" {
 * @param[in] config the pointer of pico_fatfs_spi_config_t to configure with
 */
 void pico_fatfs_set_config(pico_fatfs_spi_config_t *config);
+int pico_fatfs_reboot_spi(void);
 
 #ifdef __cplusplus
 }
